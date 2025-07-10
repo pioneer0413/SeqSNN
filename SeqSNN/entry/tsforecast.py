ae1636f979c326c7825c05dc613adf56aec10d44
pioneer0413 <hw.kang@knu.ac.kr>
2025-07-10 13:16:01 +0900

    v0.0.1.1 - measure time in tsforecast.py

--- SeqSNN/entry/tsforecast.py
+++ SeqSNN/entry/tsforecast.py
@@ -8,6 +8,8 @@ from SeqSNN.dataset import DATASETS
 from SeqSNN.runner import RUNNERS
 from SeqSNN.network import NETWORKS
 
+import time
+
 warnings.filterwarnings("ignore")
 
 
@@ -42,4 +44,24 @@ def run_train(config):
 
 if __name__ == "__main__":
     _config = SeqSNNConfig.fromcli()
+
+    time_start = time.time()
     run_train(_config)
+    execution_time = time.time() - time_start
+
+    save_path = 'outputs/execution_time.csv'
+    header = ['network_type', 'encoder_type', 'num_steps', 'execution_time']
+    record = [_config.network.type, _config.network.encoder_type, _config.network._num_steps, execution_time]
+    '''
+    if not save_path exists, then create it and write the header with CSV format
+    else append the record to the CSV file
+    '''
+    import os
+    import csv
+    if not os.path.exists(save_path):
+        with open(save_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+    with open(save_path, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(record)
