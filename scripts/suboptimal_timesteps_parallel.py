6519bd530f89cda117ef818733f1041d52c2afc1
pioneer0413 <hw.kang@knu.ac.kr>
2025-07-11 11:35:50 +0900

    v0.0.1.4

--- scripts/suboptimal_timesteps_parallel.py
+++ scripts/suboptimal_timesteps_parallel.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run SeqSNN experiments with different encoder types and timesteps.")
     parser.add_argument('--num_steps_start', type=int, default=9, help='Starting number of timesteps to use in experiments.')
     parser.add_argument('--num_steps_end', type=int, default=15, help='Ending number of timesteps to use in experiments.')
+    parser.add_argument('--dataset', type=str, default='electricity', help='Dataset to use for the experiments.')
     args = parser.parse_args()
     num_steps_start = args.num_steps_start
     num_steps_end = args.num_steps_end
@@ -20,19 +21,19 @@ if __name__ == "__main__":
 
     for T in range(num_steps_start, num_steps_end+1):
         commands.append(
-            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_electricity.yml "
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
             f"--network.encoder_type=delta --network.num_steps={T} "
-            f"--runtime.output_dir=./outputs/spikernn_electricity_encoder=delta_T={T}"
+            f"--runtime.output_dir=./outputs/spikernn_{args.dataset}_encoder=delta_T={T}"
         )
         commands.append(
-            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_electricity.yml "
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
             f"--network.encoder_type=repeat --network.num_steps={T} "
-            f"--runtime.output_dir=./outputs/spikernn_electricity_encoder=repeat_T={T}"
+            f"--runtime.output_dir=./outputs/spikernn_{args.dataset}_encoder=repeat_T={T}"
         )
         commands.append(
-            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_electricity.yml "
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
             f"--network.encoder_type=conv --network.num_steps={T} "
-            f"--runtime.output_dir=./outputs/spikernn_electricity_encoder=conv_T={T}"
+            f"--runtime.output_dir=./outputs/spikernn_{args.dataset}_encoder=conv_T={T}"
         )
 
     max_workers = 3  # 동시에 실행할 병렬 프로세스 수 (서버 사양에 맞춰 조절)
