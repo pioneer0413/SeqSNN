14ca27891653ba00dbf6c24749aadf8fa8b87ba0
pioneer0413 <hw.kang@knu.ac.kr>
2025-07-20 22:34:03 +0900

    v0.0.2.7

--- scripts/parallel_spikernn_baseline.py
+++ scripts/parallel_spikernn_baseline.py
@@ -20,27 +20,46 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', type=str, default='electricity', choices=['electricity', 'metr-la', 'pems-bay', 'solar'], help='Dataset to use for the experiments.')
     parser.add_argument('--horizon', type=int, default=24, choices=[6, 24, 48, 96], help='Forecasting horizon.')
     parser.add_argument('--seed', type=int, default=40)
+    parser.add_argument('--cluster', action='store_true', default=False, help='Run in cluster mode.')
+    parser.add_argument('--gpu_id', type=int, default=0, help='GPU ID to use for the experiments.')
     args = parser.parse_args()
 
-    dst_path = '/home/hwkang/SeqSNN/warehouse/with_pe'
+    dst_path = '/home/hwkang/SeqSNN/warehouse/cluster' if args.cluster else '/home/hwkang/SeqSNN/warehouse/with_pe'
 
     commands = []
 
-    commands.append(
-        f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
-        f"--network.encoder_type=repeat --data.horizon={args.horizon} --runtime.seed={args.seed} "
-        f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=repeat_horizon={args.horizon}_baseline_seed={args.seed}"
-    )
-    commands.append(
-        f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
-        f"--network.encoder_type=delta --data.horizon={args.horizon} --runtime.seed={args.seed} "
-        f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=delta_horizon={args.horizon}_baseline_seed={args.seed}"
-    )
-    commands.append(
-        f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
-        f"--network.encoder_type=conv --data.horizon={args.horizon} --runtime.seed={args.seed} "
-        f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=conv_horizon={args.horizon}_baseline_seed={args.seed}"
-    )
+    if args.cluster:
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_cluster_{args.dataset}.yml "
+            f"--network.encoder_type=repeat --data.horizon={args.horizon} --runtime.seed={args.seed} --network.gpu_id={args.gpu_id} "
+            f"--runtime.output_dir={dst_path}/spikernn_cluster_{args.dataset}_encoder=repeat_horizon={args.horizon}_baseline_seed={args.seed} "
+        )
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_cluster_{args.dataset}.yml "
+            f"--network.encoder_type=delta --data.horizon={args.horizon} --runtime.seed={args.seed} --network.gpu_id={args.gpu_id} "
+            f"--runtime.output_dir={dst_path}/spikernn_cluster_{args.dataset}_encoder=delta_horizon={args.horizon}_baseline_seed={args.seed} "
+        )
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_cluster_{args.dataset}.yml "
+            f"--network.encoder_type=conv --data.horizon={args.horizon} --runtime.seed={args.seed} --network.gpu_id={args.gpu_id} "
+            f"--runtime.output_dir={dst_path}/spikernn_cluster_{args.dataset}_encoder=conv_horizon={args.horizon}_baseline_seed={args.seed} "
+        )
+    else:
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
+            f"--network.encoder_type=repeat --data.horizon={args.horizon} --runtime.seed={args.seed} "
+            f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=repeat_horizon={args.horizon}_baseline_seed={args.seed}"
+        )
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
+            f"--network.encoder_type=delta --data.horizon={args.horizon} --runtime.seed={args.seed} "
+            f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=delta_horizon={args.horizon}_baseline_seed={args.seed}"
+        )
+        commands.append(
+            f"python -m SeqSNN.entry.tsforecast exp/forecast/spikernn/spikernn_{args.dataset}.yml "
+            f"--network.encoder_type=conv --data.horizon={args.horizon} --runtime.seed={args.seed} "
+            f"--runtime.output_dir={dst_path}/spikernn_{args.dataset}_encoder=conv_horizon={args.horizon}_baseline_seed={args.seed}"
+        )
 
     max_workers = 3  # 동시에 실행할 병렬 프로세스 수 (서버 사양에 맞춰 조절)
 
