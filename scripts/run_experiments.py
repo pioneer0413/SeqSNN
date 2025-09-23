0d922831a106ae67c6829f451b15e28aa876ae82
pioneer0413 <hw.kang@knu.ac.kr>
2025-09-23 16:13:25 +0900

    MODIFY: output_dir 양식 변경

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -45,7 +45,7 @@ def generate_single_command(config_path, method, dataset_name, encoder_type, hor
     else:
         output_dir = f'./warehouse/{source}/baseline/{method}_{dataset_name}_encoder={encoder_type}_horizon={horizon}_seed={seed}_p={postfix}'
         if encoder_type == 'cwconv':
-            output_dir = f'./warehouse/{source}/baseline/{method}_{dataset_name}_encoder={encoder_type}_horizon={horizon}_n_cluster={n_cluster}_d_model={d_model}_beta={beta}_seed={seed}_p={postfix}'
+            output_dir = f'./warehouse/{source}/baseline/{method}_{dataset_name}_encoder={encoder_type}_horizon={horizon}_n_cluster={n_cluster}_d_model={d_model}_beta={beta}_kc={k_c}_kt={k_t}_seed={seed}_p={postfix}'
         cmd = [
             sys.executable, '-m', 'SeqSNN.entry.tsforecast',
             config_path,
